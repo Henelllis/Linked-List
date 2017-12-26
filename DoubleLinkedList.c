e8ba9429c3a158dd0693e8813121da04182d7a47
U-Hendude-PC\Hendude <hellis1001@gmail.com>
2017-12-26 09:07:33 -0500

    this is a double linked list with updated feature for the orginal linked list in terms of reading from a file for test automation :)

--- DoubleLinkedList.c
+++ DoubleLinkedList.c
@@ -9,16 +9,19 @@
 
 typedef struct Node{
 	char id[10];
-	struct Node *ref;
+	struct Node *nextRef;
+	struct Node *prevRef;
 } Node;
 
-Node *root;
-
+Node *root_header;
+Node *root_trailer;
 
 void init(char *id_desc){
-	root = malloc(sizeof(Node));
-	strcpy( root -> id , id_desc);
-	root -> ref = NULL;
+	root_header = malloc(sizeof(Node));
+	strcpy( root_header -> id , id_desc);
+	root_header -> nextRef = NULL;
+	root_header -> prevRef = NULL;
+	root_trailer = root_header;
 }
 
 void add_node(char *id_desc){
@@ -26,20 +29,22 @@ void add_node(char *id_desc){
 	/*2  Create a new Node for the root*/
 	/*3 Populate that node*/
 	/*4 Take the tmp node and make that the ref node for the root*/
-	if(root == NULL){
+	if(root_header == NULL){
 		init(id_desc);
 	}
 	else{	
 		Node *tmp;
-		tmp = root;
-		root  = malloc(sizeof(Node));
-		strcpy( root -> id , id_desc);
-		root -> ref =  tmp;
+		tmp = root_header;
+		root_header  = malloc(sizeof(Node));
+		tmp -> nextRef = root_header;
+		strcpy( root_header -> id , id_desc);
+		root_header -> prevRef =  tmp;
+		root_header -> nextRef = NULL;
 	}
 
 }
 
-bool remove_node(char *id_desc){
+/*bool remove_node(char *id_desc){
 
 	//If Node does not exist
 	//why bother trying to remove it
@@ -62,7 +67,7 @@ bool remove_node(char *id_desc){
 	else if (strcmp(node_root -> id , id_desc) && node_root -> ref == NULL ){
 		printf("Root Node does not match and its the only Node in the chain\n");
 		return false;
-	}	
+	
 	else if(strcmp(node_root -> id, id_desc) == 0 && node_root -> ref != NULL){
 		printf("Root node is to be removed and is the first one on the chain\n");
 		root = node_ref;
@@ -91,21 +96,21 @@ bool remove_node(char *id_desc){
 
 	return false;
 
-}
+}*/
 
 bool findNode(char *id_desc){
 
-	if(root == NULL){
+	if(root_header == NULL){
 		return false;
 	}
 
 	Node *explore;
-	explore = root;
+	explore = root_header;
 	while( explore != NULL){
 		if(strcmp(explore -> id,id_desc)){
 			return true;
 		}
-		explore = explore -> ref;
+		explore = explore -> nextRef;
 	}
 
 	return false;
@@ -113,17 +118,35 @@ bool findNode(char *id_desc){
 
 void iterate_Nodes(){
 
-	if(root == NULL){
+	if(root_header == NULL){
 		printf("Nothing to iterate over\n");
 	}
 	else{
 		int counter = 0;
 		Node *explore;
-		explore = root;
+		explore = root_header;
 		while( explore != NULL){
 			printf("Node %d : %s\n",counter,explore -> id );
 			counter++;
-			explore = explore -> ref;
+			explore = explore -> prevRef;
+		}
+	}
+}
+
+
+void iterateBack_Nodes(){
+
+	if(root_trailer == NULL){
+		printf("Nothing to iterate over\n");
+	}
+	else{
+		int counter = 0;
+		Node *explore;
+		explore = root_trailer;
+		while( explore != NULL){
+			printf("Node %d : %s\n",counter,explore -> id );
+			counter++;
+			explore = explore -> nextRef;
 		}
 	}
 }
@@ -131,28 +154,14 @@ void iterate_Nodes(){
 
 int main(){
 	
-
-	printf("Im all alone\n");
- 
-	char mem = 'T';
-	char *ch;
-	scanf("%c", &ch);
-//	ch = (char *) mem;
-	while(true){
-	//while(*ch == 'T'){
-		scanf("%c", &ch);
-	//	printf("DIP DIP LITTLE DIP\n");
-		if(*ch == 'Q' || *ch =='q'){
-			printf("Quiting process, BYEEE\n");
-			return 1;
-		}
-	//	sleep(1);
-	//	printf("SHA SHA SHA NANANAAN\n");
-	}
-
-
+	add_node("tacitus");
+	add_node("Livy");
+	add_node("Horace");
+	iterate_Nodes();
+	iterateBack_Nodes();
 	
-	return 0;
+ 
+
 }
 
 
